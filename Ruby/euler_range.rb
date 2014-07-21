20ef2fd03e229b12b6d4c7150216b831115f4d6a
ClarkGH <clarkhinchcliff@hotmail.com>
2014-07-21 15:42:03 -0700

    Added basic skeleton

---
+++ Ruby/euler_range.rb
@@ -0,0 +1,14 @@
+#Pseudocode
+# Start with a max number of 100
+# find all the multiples of 3 and/or 5 below 100
+# find the sum of all the multiples of 3 or 5 below 100
+
+def range_sum(max_val)
+
+  range = (1..max_val).to_a
+
+  range.each do
+
+  end
+
+end
