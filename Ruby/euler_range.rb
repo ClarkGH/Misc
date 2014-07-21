3338b0607c75f12440735c8ef96a61260418e060
ClarkGH <clarkhinchcliff@hotmail.com>
2014-07-21 15:53:32 -0700

    Will create an array with div numbers -all numbers in the array are divisible by 3, 5, or both -there are no duplicates

--- Ruby/euler_range.rb
+++ Ruby/euler_range.rb
@@ -2,13 +2,25 @@
 # Start with a max number of 100
 # find all the multiples of 3 and/or 5 below 100
 # find the sum of all the multiples of 3 or 5 below 100
+# change it to take any value afterwards
 
 def range_sum(max_val)
 
   range = (1..max_val).to_a
 
-  range.each do
+  multiple_three_five = []
 
+  range.each do |num|
+    if num % 5 == 0 && num % 3 == 0
+      multiple_three_five << num
+    elsif num % 5 == 0 && num %3 != 0
+      multiple_three_five << num
+    elsif num % 3 == 0 && num %5 != 0
+      multiple_three_five << num
+    end
   end
-
+  multiple_three_five.sort!
+  p multiple_three_five
 end
+
+range_sum(100)
