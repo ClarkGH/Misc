2b95b9c0270cc4be122cb7c91b78791f2d9a7129
ClarkGH <clarkhinchcliff@hotmail.com>
2014-11-07 15:42:31 -0700

    set up a yeoman skeleton for angular

---
+++ Node/app/scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nodeApp
+ * @description
+ * # nodeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nodeApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
