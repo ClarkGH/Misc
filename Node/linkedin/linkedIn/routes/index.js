7a4c621d94368c6c221b93ba8be007854b22b3b9
ClarkGH <clarkhinchcliff@hotmail.com>
2014-11-21 15:33:03 -0700

    Example route and API folder added

--- Node/linkedin/linkedIn/routes/index.js
+++ Node/linkedin/linkedIn/routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+// respond with "Hello World!" on the homepage
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+})
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+// accept POST request on the homepage
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+})
 
-module.exports = router;
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+})
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+})
