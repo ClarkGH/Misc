eba0c602a78f56acdcef5deceab4561cc37d92a3
ClarkGH <clarkhinchcliff@hotmail.com>
2014-11-22 12:20:00 -0700

    cleaned up asyncio to follow tutorials conventions

--- Node/nodetut/asyncio.js
+++ Node/nodetut/asyncio.js
@@ -1,4 +1,7 @@
 var fs = require('fs');
-var contents = fs.readFile(process.argv[2], function(err, data) {
-  console.log(data.toString().split('\n').length - 1);
+var file = process.argv[2];
+
+fs.readFile(file, function (err, contents) {
+  var lines = contents.toString().split('\n').length - 1;
+  console.log(lines);
 });
