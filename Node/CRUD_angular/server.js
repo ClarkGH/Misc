f91de34c629e8d2e773b6eb72c39977135b3619c
ClarkGH <clarkhinchcliff@hotmail.com>
2014-11-24 21:07:26 -0700

    Changed directory name -configured node app

---
+++ Node/CRUD_angular/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+//server config
+mongoose.connect('mongodb://clarkgh:iloveit@proximus.modulusmongo.net:27017/tiH8ohod');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(morgan('dev'));
+// this will log every request to the console
+
+app.use(bodyParser.urlencoded({'extended':'true'}));
+//parse in x-www-form-urlencoded
+app.use(bodyParser.json());
+//parse app in json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+//parse vnd.api as json
+app.use(methodOverride());
+
+app.listen(3000);
+console.log("This app is running on port 3000");
