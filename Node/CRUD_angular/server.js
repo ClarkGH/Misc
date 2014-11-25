88c573a5da7b7822635bca5596e75c80913fe330
ClarkGH <clarkhinchcliff@hotmail.com>
2014-11-24 21:43:45 -0700

    created routes

--- Node/CRUD_angular/server.js
+++ Node/CRUD_angular/server.js
@@ -25,6 +25,56 @@ app.use(methodOverride());
 var Todo = mongoose.model('Todo', {
   test : String
 });
+//TODO routes ===========================
+  //get all
+  app.get('/api/todos', function(req, res) {
+    Todo.find(function(err, todos) {
+      if (err)
+        res.send(err)
+
+      res.json(todos);
+    });
+  });
+
+  //create todo
+  app.post('/api/todos', function(req, res) {
+
+    Todo.create({
+      text : req.body.text,
+      done : false
+    }, function(err, todo) {
+      if (err)
+        res.send(err);
+
+      //get all todos after creates
+      Todo.find(function(err, todos) {
+        if (err)
+          res.send(err)
+        res.json(todos);
+      });
+    });
+
+  });
+
+  //delete todo
+app.delete('/api/todos/:todo_id', function(req, res) {
+  Todo.remove({
+    _id : req.params.todo_id
+  }, function(err, todo) {
+    if (err)
+      res.send(err);
+
+    //get all todos after delete
+
+    Todo.find(function(err, todos) {
+      if (err)
+        res.send(err)
+      res.json(todos);
+    });
+  });
+});
+
+//==============================
 
 //Server
 app.listen(3000);
